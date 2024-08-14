5a38d84ce84dd547647b2c164c1beb99d912bed9
Casper Nilsson <caspernilsson13@gmail.com>
2024-08-14 12:03:17 +0200

    Finished the workshop tasks.

--- src/main/java/se/lexicon/exceptions/workshop/Main.java
+++ src/main/java/se/lexicon/exceptions/workshop/Main.java
@@ -1,27 +1,41 @@
 package se.lexicon.exceptions.workshop;
 
+import java.io.IOException;
 import java.util.List;
 
 import se.lexicon.exceptions.workshop.data_access.NameService;
+import se.lexicon.exceptions.workshop.domain.Gender;
 import se.lexicon.exceptions.workshop.domain.Person;
+import se.lexicon.exceptions.workshop.exception.DuplicateNameException;
 import se.lexicon.exceptions.workshop.fileIO.CSVReader_Writer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-		List <String> maleFirstNames = CSVReader_Writer.getMaleFirstNames();
-        List <String> femaleFirstNames = CSVReader_Writer.getFemaleFirstNames();
+    public static void main(String[] args) {
+        try {
+            List<String> maleFirstNames = CSVReader_Writer.getMaleFirstNames();
+            List<String> femaleFirstNames = CSVReader_Writer.getFemaleFirstNames();
 
-        List <String> lastNames = CSVReader_Writer.getLastNames();
-
-
-        NameService nameService = new NameService(maleFirstNames, femaleFirstNames,lastNames);
+            List<String> lastNames = CSVReader_Writer.getLastNames();
 
 
-        Person test = nameService.getNewRandomPerson();
-        System.out.println(test);
+            NameService nameService = new NameService(maleFirstNames, femaleFirstNames, lastNames);
 
-	}
+
+            Person test = nameService.getNewRandomPerson();
+            String firstname = "Igor";
+
+
+            nameService.addMaleFirstName(firstname);
+
+
+
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading files: " + e.getMessage());
+        } catch (DuplicateNameException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 
 }
